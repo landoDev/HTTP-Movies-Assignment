b877c0b2b78cd05bcbae8842424b7d674b59b92e
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-25 19:32:54 -0400

    added a little fun when you try to edit star wars

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useRouteMatch, useHistory, useParams } from 'react-router-dom';
+import { useRouteMatch, useHistory, useParams, Redirect } from 'react-router-dom';
 import MovieCard from './MovieCard';
 
 function Movie({ addToSavedList , movieList, setMovieList}) {
   const [movie, setMovie] = useState(null);
   const match = useRouteMatch();
   const history = useHistory()
-  // console.log(history)
 
   const fetchMovie = id => {
     axios
@@ -31,17 +30,28 @@ function Movie({ addToSavedList , movieList, setMovieList}) {
 
   const routeToUpdate = e =>{
     e.preventDefault();
-    console.log('params in route function', match.params.id)
-    history.push(`/update-movie/${match.params.id}`)
+    checkIfStarWars(movie.title);
+    {checkIfStarWars ? history.push('/'): history.push(`/update-movie/${match.params.id}`)}
+    
   }
 
   const deleteMovie = e =>{
     e.preventDefault();
-    axios.delete(`http://localhost:5000/api/movies/${match.params.id}`)
+    checkIfStarWars(movie.title);
+    {checkIfStarWars ? history.push('/'): axios.delete(`http://localhost:5000/api/movies/${match.params.id}`)
     .then(res=>{
       console.log(res)
       history.push('/')
-    })
+    })}
+
+  }
+
+  const checkIfStarWars = title =>{
+    if(title === 'Star Wars'){
+      return(
+        alert('DON\'\T YOU F$#%!@# DARE!')
+        ) 
+    } 
   }
 
   return (
